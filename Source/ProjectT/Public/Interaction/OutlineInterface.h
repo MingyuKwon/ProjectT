59f1022217edc10b561437a8317f120e0eb90141
Kwon Mingyu <kmgmg230601@gmail.com>
2024-04-11 23:25:45 +0900

    필드 이주했습니다

---
+++ Source/ProjectT/Public/Interaction/OutlineInterface.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ProjectT/ProjectT.h"
+#include "OutlineInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, BlueprintType)
+class UOutlineInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJECTT_API IOutlineInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	virtual void SetHighLightOn() = 0;
+	virtual void SetHighLightOff() = 0;
+	virtual bool GetIsHighlighted() = 0;
+
+	virtual bool GetIsOnBoard() = 0;
+	virtual void SetIsOnBoard(bool isOnBoard) = 0;
+
+	virtual void SetActorPosition(FVector Position) = 0;
+};
