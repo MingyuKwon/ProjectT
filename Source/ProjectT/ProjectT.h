7b0a0ecd5b4bb8dbe11f0285f0b4551dc0de79d1
Kwon Mingyu <kmgmg230601@gmail.com>
2024-04-11 22:58:19 +0900

    이주 전에 우선 손만 보이는 빈 필드를 생성했습니다

---
+++ Source/ProjectT/ProjectT.h
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define RED_STENCIL 250
+#define BLUE_STENCIL 251
+#define WHITE_STENCIL 252
+
+#define TRACE_LENGTH 10000
+#define ECC_FloorTraceChannel ECC_GameTraceChannel1
+#define ECC_BoardTraceChannel ECC_GameTraceChannel2
+#define ECC_OffencerFieldTraceChannel ECC_GameTraceChannel3
+#define ECC_DeffenceFieldTraceChannel ECC_GameTraceChannel4
+#define ECC_BulletTraceChannel ECC_GameTraceChannel5
+
+
+UENUM(BlueprintType)
+enum class EObjectType : uint8
+{
+	EOT_NONE,
+	EOT_ATTACKER,
+	EOT_DEFENDER,
+
+	EOT_MAX
+};
+
+
---
+++ Source/ProjectT/Public/ProjectTGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ProjectTGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTT_API AProjectTGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
